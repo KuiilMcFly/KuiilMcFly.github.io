5c43f7a039c7c9a66f6ca29ab3852dc0bd663bf1
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2022-11-04 13:22:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+gsap.fromTo(
+    "#sushifoto2",
+    { x: 50, opacity: 0 },
+    { x: 0, opacity: 1, duration: 5 }
+  );
+  gsap.fromTo(
+    "#sushifoto1",
+    { x: -50, opacity: 0 },
+    { x: 0, opacity: 1, duration: 5 }
+  );
+  gsap.fromTo(
+    "#sushifoto3",
+    { x: -50, opacity: 0 },
+    { x: 0, opacity: 1, duration: 5 }
+  );
+
+  gsap.fromTo(
+    "#sushifoto4",
+    { x: -50, opacity: 0 },
+    { x: 0, opacity: 1, duration: 5}
+  );
+
+  gsap.fromTo(
+    ".tips-box",
+    { x: 0, opacity: 0 },
+    { x: 0, opacity: 1, duration: 3}
+  );
